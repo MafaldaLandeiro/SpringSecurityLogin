37fe1c5f4d3b5ec56975f8a0309c8b047eaa188a
MafaldaLandeiro <1110590@isep.ipp.pt>
2016-03-31 23:08:29 +0100

    Spring MVC configuration

---
+++ src/main/java/org/springSecurityLogin/configuration/AppConfig.java
@@ -0,0 +1,34 @@
+package org.springSecurityLogin.configuration;
+
+import org.springSecurityLogin.exception.AccessDeniedExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "org.springSecurityLogin.*" })
+@Import({ SecurityConfiguration.class })
+public class AppConfig extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/pages/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	@Bean
+	public AccessDeniedExceptionHandler errorPage(){
+		AccessDeniedExceptionHandler errorPage = new AccessDeniedExceptionHandler("403");
+		return errorPage;
+	}
+
+}
